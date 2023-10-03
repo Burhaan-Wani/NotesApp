a8a2b7fd9a53e02538d5f95e1c4ac932daad1340
Burhaan <burhaanwani823@gmail.com>
2023-10-03 12:53:08 +0530

    now

--- NotesApp/settings.py
+++ NotesApp/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-4dhwc^zb#ig@ah+euzpvlpls0v_%-zr77!m^3d@&ivuytz&*m2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.vercel.app', '.now.sh'
+]
 
 
 # Application definition
@@ -45,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Notes',
+    'Notes', 
     "verify_email.apps.VerifyEmailConfig"
 ]
 
@@ -131,3 +133,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STAICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
