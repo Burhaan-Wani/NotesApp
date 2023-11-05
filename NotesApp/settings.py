f52b5b9c0bf5861740c5021f8185f03f3447a014
Burhaan <burhaanwani823@gmail.com>
2023-11-05 21:21:35 +0530

    commit

--- NotesApp/settings.py
+++ NotesApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-4dhwc^zb#ig@ah+euzpvlpls0v_%-zr77!m^3d@&ivuytz&*m2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
