4736ca0156a09715388c632a19f4f5172369c03b
Burhaan <burhaanwani823@gmail.com>
2023-11-08 19:54:16 +0530

    commit

--- staticfiles/js/serviceworker.js
+++ staticfiles/js/serviceworker.js
@@ -1,24 +1,22 @@
 var staticCacheName = "djangopwa-v1";
 
-self.addEventListener("install", function (event) {
-    event.waitUntil(
+self.oninstall = function (evt) {
+    evt.waitUntil(
         caches.open(staticCacheName).then(function (cache) {
-            return cache.addAll(["/Login"]);
+            return Promise.all(
+                ["/", "Login.html"].map(function (url) {
+                    return fetch(new Request(url, { redirect: "manual" })).then(
+                        function (res) {
+                            return cache.put(url, res);
+                        }
+                    );
+                })
+            );
         })
     );
-});
-
-self.addEventListener("fetch", function (event) {
-    var requestUrl = new URL(event.request.url);
-    if (requestUrl.origin === location.origin) {
-        if (requestUrl.pathname === "/") {
-            event.respondWith(caches.match("/Login"));
-            return;
-        }
-    }
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
-});
+};
+self.fetch = function (evt) {
+    var url = new URL(evt.request.url);
+    if (url.pathname != "/" && url.pathname != "Login.html") return;
+    evt.respondWith(caches.match(evt.request, { cacheName: staticCacheName }));
+};
